93c8c8d5ed2232054a176cc1d6784b53fe832fcb
Tasneem-Alatrash <tasneemalatrash4@gmail.com>
2026-08-06 11:49:44 +0300

    Add User entity and PostgreSQL database configuration

---
+++ src/main/java/com/example/SpringBoot/Model/User.java
@@ -0,0 +1,80 @@
+package com.example.SpringBoot.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "first_name" , nullable = false, length = 50)
+    private String firstName;
+
+    @Column(name = "last_name" , nullable = false, length = 50)
+    private String lastName;
+    @Column(name = "phone_number" , nullable = false , unique = true , length = 20)
+    private String phoneNumber;
+
+    @Column(name = "password" , nullable = false , length = 255)
+    private String password;
+
+    public User(){}
+
+    public User(String firstName, String lastName, String phoneNumber, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
