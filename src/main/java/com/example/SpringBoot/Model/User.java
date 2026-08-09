ee3db8ab89cf9c9dcb8fb17cd7d9f4526537f942
Tasneem-Alatrash <tasneemalatrash4@gmail.com>
2026-08-09 09:53:15 +0300

    refactor: use Lombok @Data instead of manual getters and setters

--- src/main/java/com/example/SpringBoot/Model/User.java
+++ src/main/java/com/example/SpringBoot/Model/User.java
@@ -1,9 +1,12 @@
 package com.example.SpringBoot.Model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
 
 @Entity
-@Table(name = "users")
+@Table(name = "user")
+@Data
 public class User {
 
     @Id
@@ -20,6 +23,7 @@ public class User {
     private String phoneNumber;
 
     @Column(name = "password" , nullable = false , length = 255)
+    @ToString.Exclude
     private String password;
 
     public User(){}
@@ -31,50 +35,4 @@ public class User {
         this.password = password;
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
