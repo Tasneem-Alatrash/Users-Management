983d359a374d67ba346840c46acd3a272d2ca23e
Tasneem-Alatrash <tasneemalatrash4@gmail.com>
2026-08-17 12:21:11 +0300

    style: use text blocks for multiline queries for readability

--- src/main/java/com/example/SpringBoot/Repository/QueryPracticeRepository.java
+++ src/main/java/com/example/SpringBoot/Repository/QueryPracticeRepository.java
@@ -15,13 +15,22 @@ public interface QueryPracticeRepository extends JpaRepository<User, Integer> {
     List<User> findByFirstNameAndLastName(String firstName, String lastName);
 
     // JPQL
-    @Query("SELECT u FROM User u WHERE u.firstName = :firstName AND u.lastName = :lastName")
+    @Query(""" 
+            SELECT u
+            FROM User u 
+            WHERE u.firstName = :firstName AND u.lastName = :lastName 
+            """)
     List<User> searchByNameJPQL (
             @Param("firstName") String firstName,
             @Param("lastName") String lastName);
 
     // Native
-    @Query(value = "SELECT * FROM \"user\" WHERE first_name = :firstName AND last_name = :lastName " , nativeQuery = true)
+    @Query(value = """
+        SELECT * 
+        FROM \"user\" 
+        WHERE first_name = :firstName AND last_name = :lastName 
+        """ ,
+        nativeQuery = true)
     List<User> searchByNameNative (
             @Param("firstName") String firstName,
             @Param("lastName") String lastName);
